0910b18f5d8bdb79f0e3228f9735d9b54bb3fa7a
Meidencore <danieleverdale@gmail.com>
2024-04-20 23:56:21 -0400

    01-CardDeal component ready

--- Projects/01-modern-website/src/components/Button.jsx
+++ Projects/01-modern-website/src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}>
         Get Started
     </button>
   )
